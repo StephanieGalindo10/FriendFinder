4ec3c8951b589e91b48cbd68d43515249cd81e6e
test <test@gmail.com>
2018-11-15 20:16:02 -0800

    change express var

--- sever.js
+++ sever.js
@@ -1,13 +1,15 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+var PORT = process.env.PORT || 3000;
+
+
+var path = require('path');
+var fs = require('fs');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+var PORT = process.env.PORT || 3000;
