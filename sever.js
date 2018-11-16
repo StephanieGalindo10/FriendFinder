d127c6c995c71441f09f622eb87675358f672e44
test <test@gmail.com>
2018-11-15 20:20:06 -0800

    started express app and clean code

--- sever.js
+++ sever.js
@@ -5,11 +5,15 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
-
+///create a application//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 var path = require('path');
 var fs = require('fs');
 
-// Sets up the Express App
+// Sets up the Express App contuine//
 // =============================================================
+var path = require('path');
+var fs = require('fs');
 
-var PORT = process.env.PORT || 3000;
+
