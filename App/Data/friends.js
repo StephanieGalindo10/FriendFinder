73747ffb7af7a0882e2fcaa1038e66918e39a10d
test <test@gmail.com>
2018-11-16 20:11:55 -0800

    change friends.js but need image of me

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -1,1 +1,23 @@
-//friends.js//
+//friends.js arrays//
+
+
+var friendList = [
+    {
+      name: 'Stephanie Galindo',
+      profilePic: img src="desktop" alt="profile">
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "3",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
