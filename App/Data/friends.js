9d3725a32b862d27ea32b1e4a51eae70a83b72ea
test <test@gmail.com>
2018-11-17 18:45:06 -0800

    added major changes and fix bugs

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -4,7 +4,7 @@
 var friendList = [
     {
       name: 'Stephanie Galindo',
-      profileimage: img src="desktop" alt="profile">
+      profilePic: "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/36355996_1844024292321223_5565288592021913600_n.jpg?_nc_cat=109&_nc_ht=scontent-sjc3-1.xx&oh=7c5e6694e06a26e5cc4a039e23c32ee5&oe=5C85FF86",
       scores: [
         "4",
         "5",
@@ -16,6 +16,22 @@ var friendList = [
         "4",
         "3",
         "4"
+      ]
+    },
+    {
+      name: 'Alvaro',
+      profilePic: "https://i.pinimg.com/236x/40/19/94/40199442083e85585816b9fcfe640e6b--gorgeous-men-beautiful-people.jpg",
+      scores: [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
       ]
     }
   ]
---
+++ App/routing/api-routes.js
@@ -0,0 +1,53 @@
+var friends = require('../data/friends');
+
+///API ROUTES///
+module.exports = function(app) {
+
+	// Show all friends available
+	app.get('/api/friends', function(req, res) {
+		
+	});
+
+	app.post('/api/friends', function(req, res) {
+		// closest match object.
+		var results = [];
+
+		console.log(req.body, "this is the new friend");
+
+		// string of JSON info
+		var postResponse = JSON.stringify(req.body);
+
+		
+
+		    // Store the difference in values
+		    var closestMatch = 0;
+		    var matchScore = 999999999999999;
+
+		    // Loop through the file to find the closest match
+		    for (var i = 0; i < friends.length; i++) {
+		    	var spaceBetween = 0;
+		    	for (var j = 0; j < friends[i].scores.length; j++) {
+		    		spaceBetween += Math.abs((parseInt(req.body.scores[j]) - parseInt(friends[i].scores[j])));
+				}
+
+				// If the space between the current listing is the closest to the user, update the closestMatch
+				if(spaceBetween <= matchScore) {
+					matchScore = spaceBetween;
+					closestMatch = i;
+		    	}
+		    }
+
+		    // console.log("Closest match: " + friendFile[closestMatch].name);
+
+		    results.push(friends[closestMatch]);
+
+		    // Add the new person to the existing array
+		
+			friends.push(req.body);
+		    // Push back the entire updated result immediately
+		   
+			res.send(results[0]);
+
+		});
+	
+}
